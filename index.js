44ba26972662106e95e16ab2689bac3765e13257
kanephamphu <phamphutaitrainer@gmail.com>
2020-11-13 22:06:40 +0700

    T

--- index.js
+++ index.js
@@ -457,7 +457,7 @@ io.sockets.on('connection',function(socket){
 				time_type : 'required',
 				from_time : 'required'
 			});
-			const matched = v.check();
+			const matched = await v.check();
 			if(matched){
 				jwt.verify(data.secret_key,process.env.login_secret_key,async (err,decoded)=>{
 					if(err){
