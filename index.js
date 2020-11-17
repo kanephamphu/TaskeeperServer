9c341ff38d1fae52acf18ba262bf738bf1689f9d
kanephamphu <phamphutaitrainer@gmail.com>
2020-11-17 12:12:47 +0700

    Tsdf

--- index.js
+++ index.js
@@ -949,6 +949,7 @@ io.sockets.on('connection',function(socket){
 			});
 			const matched = await v.check();
 			if(matched){
+				console.log(data.day_of_birth);
 				jwt.verify(data.secret_key,process.env.login_secret_key,async (err,decoded)=>{
 					if(err){
 						socket.emit("sv-edit-info",{"success":false, "errors":{"message": "Token error", "rule" : "token"}});
@@ -957,7 +958,7 @@ io.sockets.on('connection',function(socket){
 						if(await checkExist(decoded._id) == false){
 							addToList(decoded._id, socket.id);
 						}
-						console.log(day_of_birth)
+						console.log(data.day_of_birth);
 						let result = await userController.editPersonalInfo(decoded._id,data.first_name, data.last_name, data.email,data.phone_number, data.gender, data.day_of_birth, data.month_of_birth, data.year_of_birth);
 						socket.emit("sv-edit-info", result);
 					}
