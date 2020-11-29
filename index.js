fe138765cc3532c1d8fa4f7791042a1c28cbaf77
kanephamphu <phamphutaitrainer@gmail.com>
2020-11-29 15:17:29 +0700

    Tsdfsfdsf

--- index.js
+++ index.js
@@ -1097,10 +1097,12 @@ io.sockets.on('connection',function(socket){
 						if(await checkExist(decoded._id) == false){
 							addToList(decoded._id, socket.id);
 						}
-						tasksController.recommendTask(decoded._id, (data)=>{
-							console.log(data);
-							socket.emit("sv-get-recommend-task", data);
-						});
+						let result = await tasksController.recommendTask(decoded._id);
+						if(result){
+							socket.emit("sv-get-recommend-task", result);
+						}else{
+							socket.emit("sv-get-recommend-task", {"success" : false});
+						}
 					}
 				})
 			}else{
