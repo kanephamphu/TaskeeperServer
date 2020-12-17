e1bec0888b161269cbecffeb888d07e8ba3aa1f0
kanephamphu <phamphutaitrainer@gmail.com>
2020-12-18 00:13:00 +0700

    Taa

--- models/NewsModel.js
+++ models/NewsModel.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.connect(process.env.mongo_URL || "mongodb+srv://tai123:tai123@cluster0.fsksm.gcp.mongodb.net/Taskeeper?retryWrites=true&w=majority");
 var News= new mongoose.Schema({
-    _id : false,
     user_id : {
         type: String,
         index : true,
