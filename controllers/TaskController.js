099537c578c831be5a60991f74e25bdea746aec9
kanephamphu <phamphutaitrainer@gmail.com>
2020-12-20 00:25:30 +0700

    Approve

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -467,33 +467,29 @@ async function getTaskManage(task_owner_id, number_task, skip){
 //getTaskManage("5fb378656eae3400041711a3",10,0)
 // Client send approve work 
 async function approveEmployeeToWork(task_owner_id, task_id, employee_id){
-    try{
-        let pricelist = await task.findOne({"_id" : task_id, "task_owner_id" : task_owner_id, "task_candidate_apply_list.candidate_id" : employee_id}, ["task_candidate_apply_list"]);
-        if(pricelist){
-            let price = await pricelist.task_candidate_apply_list.price;
-            let user = await userController.getInformation(employee_id);
-            let result = await task.updateOne({"_id" : task_id, "task_owner_id" : task_owner_id, "task_candidate_apply_list.candidate_id" : employee_id},
-            {
-                $push : {
-                    "work_employee_list" : {
-                        "employee_id" : employee_id,
-                        "price" : price,
-                        "employee_first_name" : user.first_name,
-                        "employee_last_name" : user.last_name,
-                        "employee_avatar" : user.avatar
-                    }
+    let pricelist = await task.findOne({"_id" : task_id, "task_owner_id" : task_owner_id, "task_candidate_apply_list.candidate_id" : employee_id}, ["task_candidate_apply_list"]);
+    if(pricelist){
+        let price = await pricelist.task_candidate_apply_list.price;
+        let user = await userController.getInformation(employee_id);
+        let result = await task.updateOne({"_id" : task_id, "task_owner_id" : task_owner_id, "task_candidate_apply_list.candidate_id" : employee_id},
+        {
+            $push : {
+                "work_employee_list" : {
+                    "employee_id" : employee_id,
+                    "price" : price,
+                    "employee_first_name" : user.first_name,
+                    "employee_last_name" : user.last_name,
+                    "employee_avatar" : user.avatar
                 }
-            });
-            if(result !== null){
-                return {"success" : true};
-            }else{
-                return {"success" : false};
             }
+        });
+        if(result !== null){
+            return {"success" : true};
+        }else{
+            return {"success" : false};
         }
-        return {"success" : false};
-    }catch(err){
-        throw("Apply error");
     }
+    return {"success" : false};
 }
 //approveEmployeeToWork()
 
